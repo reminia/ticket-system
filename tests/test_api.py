68c8229728faaa6efcfa57058cdca7105d5b4ecf
sleefd <sleefd@gmail.com>
2024-07-15 20:03:50 +0800

    test: use pytest-mock to mock db & queue behaviour

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,36 @@
+import uuid
+from datetime import datetime
+
+import pytest
 from fastapi.testclient import TestClient
 
 from src.main import app
+from src.models.database import get_db
+from src.models.schemas import TicketStatus
+from src.models.ticket import Ticket
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def mock_db_session(mocker):
+    # Create a mock DB session
+    return mocker.Mock()
+
+
+@pytest.fixture(autouse=True)
+def override_get_db(mock_db_session):
+    # Override the get_db dependency to return the mock DB session
+    app.dependency_overrides[get_db] = lambda: mock_db_session
+
+
+@pytest.fixture
+def mock_ticket():
+    return Ticket(id=uuid.uuid4(),
+                  subject="test",
+                  body="test",
+                  customer_email="test@email.com",
+                  created_at=datetime.utcnow())
 
 
 def test_root_ping():
@@ -11,13 +39,19 @@ def test_root_ping():
     assert response.json() == {"status": "ok", "message": "I'm up!"}
 
 
-def test_create_ticket():
+def test_create_ticket(mocker):
+    save_ticket = mocker.patch("src.api.v1.ticket_api.save_ticket")
+    enqueue_ticket = mocker.patch("src.api.v1.ticket_api.enqueue_ticket")
+
     response = client.post("/v1/ticket",
                            json={
                                "subject": "Ticket title",
                                "body": "Ticket body",
                                "customer_email": "user@example.com"
                            })
+
+    assert save_ticket.call_count == 1
+    assert enqueue_ticket.call_count == 1
     assert response.status_code == 201
     data = response.json()
     assert data["status"] == "submitted"
@@ -25,20 +59,48 @@ def test_create_ticket():
     assert "message" in data
 
 
-def test_get_ticket():
-    response = client.post("/v1/ticket",
-                           json={
-                               "subject": "Ticket title",
-                               "body": "Ticket body",
-                               "customer_email": "user@example.com"
-                           })
-    ticket_id = response.json()["ticket_id"]
-    query_response = client.get("/v1/ticket/%s" % ticket_id)
+def test_get_ticket(mocker, mock_ticket, mock_db_session):
+    get_ticket = mocker.patch("src.api.v1.ticket_api.query_ticket")
+    get_ticket.return_value = mock_ticket
+
+    query_response = client.get("/v1/ticket/%s" % mock_ticket.id)
     assert query_response.status_code == 200
+    get_ticket.assert_called_once_with(mock_db_session, mock_ticket.id)
 
 
-def test_get_non_exist_ticket():
-    ticket_id = "9f091a6b-bc82-4413-b448-35be137884d0"
-    response = client.get(f"/v1/ticket/{ticket_id}")
+def test_get_non_exist_ticket(mocker, mock_db_session):
+    get_ticket = mocker.patch("src.api.v1.ticket_api.query_ticket")
+    get_ticket.return_value = None
+
+    ticket_id = uuid.uuid4()
+    response = client.get(f"/v1/ticket/{str(ticket_id)}")
+
     assert response.status_code == 404
     assert 'detail' in response.json()
+
+
+def test_process_ticket(mocker, mock_ticket, mock_db_session):
+    mock_filter = mocker.patch("src.api.v1.ticket_api.filter_ticket_status")
+    mock_filter.return_value = [mock_ticket, mock_ticket]
+
+    mock_enqueue = mocker.patch("src.api.v1.ticket_api.enqueue_tickets")
+    job_id = uuid.uuid4()
+    mock_enqueue.return_value.id = job_id
+
+    response = client.post("/v1/process")
+
+    assert response.status_code == 200
+    json = response.json()
+    assert json['job_id'] == str(job_id)
+    assert f"{len(mock_filter.return_value)} tickets" in json['message']
+    mock_filter.assert_called_once_with(mock_db_session, TicketStatus.SUBMITTED)
+
+
+def test_process_empty_ticket(mocker):
+    mock_filter = mocker.patch("src.api.v1.ticket_api.filter_ticket_status")
+    mock_filter.return_value = []
+
+    response = client.post("/v1/process")
+
+    assert mock_filter.call_count == 1
+    assert response.status_code == 404
